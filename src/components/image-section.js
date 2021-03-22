a1d91e718f25ebd20a1eee99760daae5e213f99e
fannyjohaaansson <fanny.johansson@hyperisland.se>
2021-03-22 09:00:23 +0100

    added some layout on the frontpage + placeholder imges.

--- src/components/image-section.js
+++ src/components/image-section.js
@@ -1,14 +1,22 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
+import image1 from '../images/ipad-work.jpg'
+import image2 from '../images/placeholder.jpg'
+import image3 from '../images/bruno-kelzer-Dw6tBa20afk-unsplash.jpg'
 
 function imageSection() {
 	return (
 		<div className="imageContainer">
-			<div classname="imageLeft">Left</div>
-			<div classname="imageRight">
-				<div className="imageUpper">upper</div>
-				<div className="imageBottom">upper</div>
-				Right
+			<div className="imageLeft">
+				<img src={image1}></img>
+			</div>
+			<div className="imageRight">
+				<div className="imageUpper">
+					<img src={image2}></img>
+				</div>
+				<div className="imageBottom">
+					<img src={image3}></img>
+				</div>
 			</div>
 		</div>
 	)
