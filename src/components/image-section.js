5b4a9149b57a76e25e85b5d331f3f22a59243790
fannyjohaaansson <fanny.johansson@hyperisland.se>
2021-03-25 09:37:49 +0100

    Image text on frontpage

--- src/components/image-section.js
+++ src/components/image-section.js
@@ -6,19 +6,22 @@ import image3 from '../images/bruno-kelzer-Dw6tBa20afk-unsplash.jpg'
 
 function imageSection() {
 	return (
-		<div className="imageContainer">
+		<section className="imageContainer">
 			<div className="imageLeft">
 				<img src={image1}></img>
+				<h5 className="imgText">Creative Stuff</h5>
 			</div>
 			<div className="imageRight">
 				<div className="imageUpper">
 					<img src={image2}></img>
+					<h5 className="imgText">Creative Stuff</h5>
 				</div>
 				<div className="imageBottom">
 					<img src={image3}></img>
+					<h5 className="imgText">Creative Stuff</h5>
 				</div>
 			</div>
-		</div>
+		</section>
 	)
 }
 
